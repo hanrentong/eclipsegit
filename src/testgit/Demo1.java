b3238f8dbe2f11e587cfbaaba0dbb0a2dece6413
Your Name <you@example.com>
2019-09-12 09:51:38 +0800

    add to note info

--- src/testgit/Demo1.java
+++ src/testgit/Demo1.java
@@ -1,5 +1,12 @@
 package testgit;
-
+/**
+ * <B>系统名称：测试git的版本切换</B><BR>
+ * <B>模块名称：</B><BR>
+ * <B>中文类名：</B><BR>
+ * <B>概要说明：</B><BR>
+ * @author bhz（11202）
+ * @since 2019年9月12日
+ */
 public class Demo1 {
 
 	public static void main(String[] args) {
